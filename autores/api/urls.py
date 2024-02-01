ab59e7345174a475c6310e428e7ffa0bb9304175
Nimrod León <nleonc14@gmail.com>
2024-02-01 12:19:40 -0500

    refactoring urls

--- autores/api/urls.py
+++ autores/api/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import AutorListCreateView, AutorDetailView, LibroListCreateView, LibroDetailView
 
 urlpatterns = [
-    path('authors/', AutorListCreateView.as_view()),
-    path('authors/<int:pk>/', AutorDetailView.as_view()),
-    path('books/', LibroListCreateView.as_view()),
-    path('books/<int:pk>/', LibroDetailView.as_view()),
+    path('autores/', AutorListCreateView.as_view()),
+    path('autores/<int:pk>/', AutorDetailView.as_view()),
+    path('libros/', LibroListCreateView.as_view()),
+    path('libros/<int:pk>/', LibroDetailView.as_view()),
 ]
